8b4b522b4f27fee66e2687d35a667f0d2e4d6270
Michael Buzzard <m.buzzard87@gmail.com>
2022-05-01 22:20:04 -0400

    add assets directory

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 
 export default function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
+        <h1>Hi</h1>
+
+)
 }
